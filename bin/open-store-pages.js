3968deb54b0c81c6d11c9416541cf68009a5a80e
Cside <cside.story@gmail.com>
2023-09-11 10:44:16 +0900

    add bin

---
+++ bin/open-store-pages.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import { cyan } from 'colorette';
+import fs from 'fs';
+import open from 'open';
+import process from 'process';
+import { URL_OF, format, getReviewUrl } from '../module/index.js';
+const ids = JSON.parse(fs.readFileSync(process.cwd() + '/ids.json').toString());
+await open(getReviewUrl(ids.chrome.id, ids));
+if (ids.edge) {
+    await open(getReviewUrl(ids.edge.crxId, ids));
+}
+else {
+    console.log(cyan('Edge is not registered.'));
+}
+if (ids.firefox) {
+    await open(getReviewUrl(ids.firefox.id, ids));
+}
+else {
+    console.log(cyan('Firefox is not registered.'));
+}
+if (ids.greasyFork) {
+    await open(format(URL_OF.GREASY_FORK.STORE, { id: ids.greasyFork.id }));
+}
+else {
+    console.log(cyan('Greasy Fork is not registered.'));
+}
