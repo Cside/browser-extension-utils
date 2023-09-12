1e9a47a82bf30665e8ea73fe624600ad8401bd5e
Cside <cside.story@gmail.com>
2023-09-12 20:12:13 +0900

    use tsup

--- module/i18n.js
+++
@@ -1,7 +0,0 @@
-export const localizeHtml = () => {
-    const documentElement = document.documentElement;
-    const innerHtmlStr = documentElement.innerHTML.toString();
-    const newInnerHtmlStr = innerHtmlStr.replace(/__MSG_(\w+)__/g, (_match, p1) => chrome.i18n.getMessage(p1));
-    if (newInnerHtmlStr != innerHtmlStr)
-        documentElement.innerHTML = newInnerHtmlStr;
-};
