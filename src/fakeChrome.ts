868f56d81bfbae21c6eabf050049da9cea151c0a
Cside <cside.story@gmail.com>
2023-12-07 09:27:48 +0900

    fix

--- src/fakeChrome.ts
+++ src/fakeChrome.ts
@@ -1,20 +1,13 @@
 import { fakeStorage } from './fakeChrome/fakeStorage';
 
-const storage = {
-  clear: fakeStorage.clear,
-  get: fakeStorage.get,
-  remove: fakeStorage.remove,
-  set: fakeStorage.set,
-};
-
 export const fakeChrome = {
   runtime: {
     getURL: (path: string) => `chrome://<extensionId>/${path}`,
     id: '<extensionId>',
   },
   storage: {
-    local: storage,
-    sync: storage,
+    local: fakeStorage,
+    sync: fakeStorage,
   },
   i18n: {
     getMessage: (name: string) => name,
