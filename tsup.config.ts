6125abef42ecbeb72fd0300f4eeec7a2d1786c00
Cside <cside.story@gmail.com>
2024-01-21 11:42:50 +0900

    cjs のサポートを切ってみる

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
   {
     entry: ['src/index.ts', 'src/bin'],
-    format: ['esm', 'cjs'],
+    format: ['esm'],
     clean: true,
     dts: true,
+    treeshake: true,
   },
 ]);
